964858525a9f3e2d7ff65ca086eb1d2c10aa1bd1
Jim <jim.pollaro@gmail.com>
2017-09-19 15:25:04 -0700

    After completing books/authors

---
+++ main/apps/dojoNinjas/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.TextField()
+
+class ninjas(models.Model):
+    dojo = models.ForeignKey(dojos,related_name = 'ninjas')
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    books = models.ManyToManyField(book,related_name='authors')
